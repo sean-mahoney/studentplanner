b4866fdc04d32e372c48a014fa37045a24dab99a
sean-mahoney <51128952+sean-mahoney@users.noreply.github.com>
2021-07-29 23:59:59 +0100

    commit stable

--- frontend/src/components/ToDoDash.js
+++ frontend/src/components/ToDoDash.js
@@ -1,29 +1,37 @@
 import React from "react";
 import Axios from "axios"; //import axios
 import { Link } from "react-router-dom";
-import Test from "./Test.js";
 
 class ToDoDash extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       lists: [],
-      currentID: 0,
+      currentUser: window.localStorage.currentuser,
     };
   }
   componentDidMount() {
-    Axios.get("http://localhost:3001/login").then((response) => {
-      //if there is a cookie present
-      if (response.data.loggedIn === true) {
-        //set login status to true and return the username
-        this.setState({ currentID: response.data.user[0].username });
-      }
+    Axios.post("http://localhost:3001/getLists", {
+      currentUser: this.state.currentUser,
+    }).then((response) => {
+      this.setState({ lists: response.data });
     });
   }
   render() {
     return (
       <div className="ToDoDash">
-        <Test />
+        <Link to="/ToDoLists">
+          <h2>To-Do Lists</h2>
+          <div className="ToDoDash-lists">
+            {this.state.lists.map((val, key) => {
+              return (
+                <div>
+                  <p>{val.list}</p>
+                </div>
+              );
+            })}
+          </div>
+        </Link>
       </div>
     );
   }
