68d3f3e4324a5894d97b1c21e239ac4a098f9b7a
sean-mahoney <51128952+sean-mahoney@users.noreply.github.com>
2021-07-31 16:43:33 +0100

    COMMIT

---
+++ frontend/src/pages/StudyPlans.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios"; //import axios
+import { AiTwotoneDelete, AiFillEdit } from "react-icons/ai";
+
+function StudyPlans() {
+  const [Plans, setPlans] = useState([]);
+  const [currentID, setCurrentID] = useState(0);
+  const [currentUser, setCurrentUser] = useState("");
+
+  useEffect(() => {
+    effect;
+    return () => {
+      cleanup;
+    };
+  }, [input]);
+
+  return <div></div>;
+}
+
+export default StudyPlans;
